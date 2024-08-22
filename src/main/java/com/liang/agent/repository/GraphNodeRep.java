485ce82a55d9b88d218e6f1312eb577417956378
Tony_Liang <166464462+TtonyLiang@users.noreply.github.com>
2024-08-22 17:13:36 +0800

    Add files via upload

---
+++ src/main/java/com/liang/agent/repository/GraphNodeRep.java
@@ -0,0 +1,21 @@
+package com.liang.agent.repository;
+
+import com.liang.agent.dto.GraphNode;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.List;
+
+/**
+ * @program: agent
+ * @ClassName GraphNodeRep
+ * @description:
+ * @author: liangliang
+ * @create: 2024-08-14 11:11
+ * @Version 1.0
+ **/
+//public interface GraphNodeRep extends Neo4jRepository<GraphNode,Long> {
+//    @Query("MATCH (n) RETURN n")
+//    List<GraphNode> findAllNodes();
+//}
